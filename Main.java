9c44c6e1b159cb71493de326e6830b7ba0090d07
Javier Asencio <108373555+JaviAsencio@users.noreply.github.com>
2022-09-21 17:43:19 +0000

    Added a scanner to read the input that someone makes in the console.

--- Main.java
+++ Main.java
@@ -1,7 +1,21 @@
-class Main {
+import java.util.Scanner;
+
+public class Main {
+
   public static void main(String[] args) {
-    System.out.println("Enter a word:");
+
+    // Read the the input by using a Scanner.
+    Scanner scanner = new Scanner(System.in);
+
+    // Display in the console a line to write in
+    System.out.println("Write message here: ");
+
+    // String the message by using a String
+    String message = scanner.nextLine();
+
+    // Print out the message that someone wrote.
+    System.out.println("You entered:");
+    System.out.println(message);
     
-    //Your code will return the length of the entered String
   }
 }
